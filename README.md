44ee7a07544a693385c2170eac1a4318189fa8f5
GreatYYX <bigyyx@gmail.com>
2016-11-08 13:49:49 -0800

    add multi-uri mapping

---
+++ README.md
@@ -0,0 +1,18 @@
+Based on [Pubby](http://wifo5-03.informatik.uni-mannheim.de/pubby/) 0.3.3.
+
+It supports requesting multi-URI-base (either sub-domain or URL path) data in same sparql query point.
+
+1. Forward URI Base to pubby (with prefix),
+e.g., `http://data.crystalbridges.org/` is forwarded to `http://data.americanartcollaborative.org/cbm/`
+
+2. In `config.ttl`, add mapping rules:
+    ```	
+    conf:webBase <http://data.americanartcollaborative.org/>
+        conf:dataset [
+     	...
+        conf:multiURIMapping [
+            conf:multiURIPrefix "cbm";
+            conf:multiURIBase "http://data.crystalbridges.org/";
+        ];
+    ];
+    ```
---
+++ src/de/fuberlin/wiwiss/pubby/MultiURIMapping.java
@@ -0,0 +1,31 @@
+package de.fuberlin.wiwiss.pubby;
+
+import java.util.HashMap;
+
+public class MultiURIMapping {
+	private HashMap<String, String> prefixMap;
+	private HashMap<String, String> baseMap;
+
+	public MultiURIMapping() {
+		prefixMap = new HashMap<String, String>();
+		baseMap = new HashMap<String, String>();
+	}
+
+	public void add(String prefix, String base) {
+		prefixMap.put(prefix, base);
+		baseMap.put(base, prefix);
+	}
+
+	public String getURIBase(String prefix) {
+		if(prefixMap.containsKey(prefix)) return prefixMap.get(prefix);
+		return null;
+	}
+
+	public String getPrefix(String URI) {
+		for(String key : baseMap.keySet()) {
+			if(URI.startsWith(key))
+				return baseMap.get(key);
+		}
+		return null;
+	}
+}
